fcb802a1f7384ac7b18df9e03c99067aae09cee7
cutsea110 <cutsea110@gmail.com>
2023-08-13 11:00:45 +0900

    refine

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,32 @@ struct User {
     email: String,
 }
 
-async fn collect_users(conn: &mut PgConnection) -> sqlx::Result<Vec<User>> {
-    conn.transaction(|txn| {
-        Box::pin(async move {
-            sqlx::query_as("SELECT * FROM users")
-                .fetch_all(&mut **txn)
-                .await
-        })
-    })
-    .await
+struct Usecase {
+    conn: PgConnection,
+}
+
+impl Usecase {
+    async fn collect_users(&mut self) -> sqlx::Result<Vec<User>> {
+        self.conn
+            .transaction(|txn| {
+                Box::pin(async move {
+                    sqlx::query_as("SELECT * FROM users")
+                        .fetch_all(&mut **txn)
+                        .await
+                })
+            })
+            .await
+    }
 }
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db_url =
         std::env::var("DATABASE_URL").expect("Env var DATABASE_URL is required. for this test");
-    let mut conn = PgConnection::connect(&db_url).await?;
-    let users = collect_users(&mut conn).await?;
+    let conn = PgConnection::connect(&db_url).await?;
+    let mut usecase = Usecase { conn };
+
+    let users = usecase.collect_users().await?;
     println!("{:#?}", users);
 
     Ok(())
