bbc52e2924718a20cb8184edf94a2b7c58b4536a
cutsea110 <cutsea110@gmail.com>
2024-01-03 10:10:42 +0900

    refactor abort

--- src/main.rs
+++ src/main.rs
@@ -156,13 +156,13 @@ where
     }
 }
 
-fn abort<Ctx, F, G>(f: F, g: G) -> impl FnOnce(&mut Ctx) -> Result<F::Item, F::Err>
+fn abort<Ctx, Tx1, F>(tx1: Tx1, f: F) -> impl FnOnce(&mut Ctx) -> Result<Tx1::Item, Tx1::Err>
 where
-    F: Tx<Ctx>,
-    G: FnOnce(F::Item) -> F::Err,
+    Tx1: Tx<Ctx>,
+    F: FnOnce(Tx1::Item) -> Tx1::Err,
 {
-    move |ctx| match f.run(ctx) {
-        Ok(t) => Err(g(t)),
+    move |ctx| match tx1.run(ctx) {
+        Ok(t) => Err(f(t)),
         Err(e) => Err(e),
     }
 }
